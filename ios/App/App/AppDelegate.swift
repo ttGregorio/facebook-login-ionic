ca853c224091d3802789cbeb7c1bcd647bb6190f
Thiago Gregorio <thiagotgregorio@gmail.com>
2021-02-28 16:03:48 -0300

    inclusao dos fontes do modelo de login no facebook com ionic

--- ios/App/App/AppDelegate.swift
+++ ios/App/App/AppDelegate.swift
@@ -5,6 +5,8 @@ import FBSDKCoreKit
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
  
+    var window: UIWindow?
+    
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
     FBSDKCoreKit.ApplicationDelegate.shared.application(application, didFinishLaunchingWithOptions: launchOptions)
     // Override point for customization after application launch.
