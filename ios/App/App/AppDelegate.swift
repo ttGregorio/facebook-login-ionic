60fe842381f8ab2f26178091b18fcb27de652654
Thiago Gregorio <thiagotgregorio@gmail.com>
2021-02-28 15:36:21 -0300

    inclusao dos fontes do modelo de login no facebook com ionic

---
+++ ios/App/App/AppDelegate.swift
@@ -0,0 +1,24 @@
+import UIKit
+import Capacitor
+import FBSDKCoreKit
+ 
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+ 
+  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    FBSDKCoreKit.ApplicationDelegate.shared.application(application, didFinishLaunchingWithOptions: launchOptions)
+    // Override point for customization after application launch.
+    return true
+  }
+ 
+  func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
+    // Called when the app was launched with a url. Feel free to add additional processing here,
+    // but if you want the App API to support tracking app url opens, make sure to keep this call
+    if CAPBridge.handleOpenUrl(url, options) {
+        return FBSDKCoreKit.ApplicationDelegate.shared.application(app, open: url, options: options)
+      }
+      else{
+       return false
+      }
+  }
+}
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,11 @@
 import { Component } from '@angular/core';
-
+import { FacebookLoginPlugin } from '@capacitor-community/facebook-login';
+import { Plugins, registerWebPlugin } from '@capacitor/core';
+import { isPlatform } from '@ionic/angular';
+import { HttpClient } from '@angular/common/http';
+ 
+import { FacebookLogin } from '@capacitor-community/facebook-login';
+registerWebPlugin(FacebookLogin);
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
@@ -7,6 +13,61 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
-
+  fbLogin: FacebookLoginPlugin;
+  user = null;
+  token = null;
+ 
+  constructor(private http: HttpClient) {
+    this.setupFbLogin();
+  }
+ 
+  async setupFbLogin() {
+    if (isPlatform('desktop')) {
+      this.fbLogin = FacebookLogin;
+    } else {
+      // Use the native implementation inside a real app!
+      const { FacebookLogin } = Plugins;
+      this.fbLogin = FacebookLogin;
+    } 
+  }
+ 
+  async login() {
+    const FACEBOOK_PERMISSIONS = ['email', 'user_birthday'];
+    const result = await this.fbLogin.login({ permissions: FACEBOOK_PERMISSIONS });
+ 
+    if (result.accessToken && result.accessToken.userId) {
+      this.token = result.accessToken;
+      this.loadUserData();
+    } else if (result.accessToken && !result.accessToken.userId) {
+      // Web only gets the token but not the user ID
+      // Directly call get token to retrieve it now
+      this.getCurrentToken();
+    } else {
+      // Login failed
+    }
+  }
+ 
+  async getCurrentToken() {    
+    const result = await this.fbLogin.getCurrentAccessToken();
+ 
+    if (result.accessToken) {
+      this.token = result.accessToken;
+      this.loadUserData();
+    } else {
+      // Not logged in.
+    }
+  }
+ 
+  async loadUserData() {
+    const url = `https://graph.facebook.com/${this.token.userId}?fields=id,name,picture.width(720),birthday,email&access_token=${this.token.token}`;
+    this.http.get(url).subscribe(res => {
+      this.user = res;
+    });
+  }
+ 
+  async logout() {
+    await this.fbLogin.logout();
+    this.user = null;
+    this.token = null;
+  }
 }
